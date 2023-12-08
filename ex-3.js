5c9b6881c2955ecaa0aa5f0d771b9740741effb7
Pao <chaiwatt77@gmail.com>
2023-12-08 14:14:44 +0700

    ex1-5

--- ex-3.js
+++ ex-3.js
@@ -8,9 +8,11 @@ const todos = [
 
 function getTodoTopics(todos) {
   // Start coding here
+	return todos.topic;
 }
 
-getTodoTopics(todos);
+let topicOfTodos = todos.map(getTodoTopics);
+console.log(topicOfTodos);
 
 /* 
 	Output:
